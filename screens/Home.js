baf7ba4d0533a3401297df6735ee1ef90ac50dad
alexzrmn <alexisrmnpro@gmail.com>
2021-02-03 13:50:11 +0100

    filter card location (done), gender (in progress)

--- screens/Home.js
+++ screens/Home.js
@@ -7,6 +7,9 @@ import * as firebase from 'firebase';
 import * as Location from 'expo-location';
 import {GeoFire} from "geofire";
 
+import filter from '../modules/filter';
+
+
 require('@firebase/database');
 
 export default class Home extends Component {
@@ -16,31 +19,48 @@ export default class Home extends Component {
     this.state = {
       profileIndex: 0,
       profiles: [],
-      user: this.props.route.params.user
+      user: this.props.route.params.user,
+      isMounted: false
     };
-    const {uid} = this.props.route.params.user;
+
+    const {uid} = this.state.user;
     this.updateUserLocation(uid);
-    this.getProfiles(uid);
+    firebase.database().ref('users').child(uid).on('value', snap => {
+      const user = snap.val()
+      this.setState({
+        user,
+        profiles: [],
+        profileIndex: 0
+      })
+      console.log(`user updated`);
+      this.getProfiles(user.uid, user.distance);
+    })
+  }
+
+  componentDidMount() {
+    this.setState({isMounted: true});
+    this.state.profiles;
   }
 
   getUser = (uid) => {
-    return firebase.database().ref('users').child(uid).once('value')
+    return firebase.database().ref('users').child(uid).once('value');
   }
 
-  getProfiles = async (uid) => {
+  getProfiles = async (uid, distance) => {
     const geofireRef = new GeoFire(firebase.database().ref('geoData'));
     const userLocation = await geofireRef.get(uid);
     console.log('userLocation', userLocation);
     const geoQuerry = geofireRef.query({
       center: userLocation,
-      radius: 10
+      radius: distance
     });
     geoQuerry.on('key_entered', async (uid, location, distance) => {
       console.log(`${uid} at ${location} is ${distance} km from the center`);
       const user = await this.getUser(uid);
       console.log(user.val().name);
       const profiles = [...this.state.profiles, user.val()];
-      this.setState({profiles});
+      const filtered = filter(profiles, this.state.user)
+      this.setState({profiles: filtered});
     })
   }
 
@@ -70,7 +90,7 @@ export default class Home extends Component {
     const {profileIndex} = this.state
     return (
       <View style={{flex:1}}>
-        {this.state.profiles.slice(profileIndex, profileIndex + 3).reverse().map((profile) => {
+        {this.state.profiles.slice(profileIndex, profileIndex + 5).reverse().map((profile) => {
           return (
             <Card
               key={profile.id}
