0b7680c5f72c57832b67ebfbb73fd8532994ede7
alexzrmn <alexisrmnpro@gmail.com>
2021-02-03 14:43:54 +0100

    filter by age

--- modules/filter.js
+++ modules/filter.js
@@ -1,20 +1,34 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import moment from 'moment';
 
 export default (profiles, user) => {
   const rejectMe = _.reject(profiles, profile => profile.uid === user.uid)
 
   const filterGender = _.filter(rejectMe, profile => {
-    const userShowMen = user.showMen && profile.gender === 'male'
-    const userShowWomen = user.showWomen && profile.gender === 'female'
+    const userShowMen = user.showMen && profile.gender === 'male';
+    const userShowWomen = user.showWomen && profile.gender === 'female';
 
-    const profileShowMen = profile.showMen && user.gender === 'male'
-    const profileShowWomen = profile.showWomen && user.gender === 'female'
+    const profileShowMen = profile.showMen && user.gender === 'male';
+    const profileShowWomen = profile.showWomen && user.gender === 'female';
 
     console.log(`Men : ${userShowMen}`, `Women : ${userShowWomen}`)
 
     return (userShowMen || userShowWomen) && (profileShowMen || profileShowWomen)
+    })
     
-  })
+    const userBirthday = moment(user.birthday, 'MM/DD/YYYY');
+    const userAge = moment().diff(userBirthday, 'years');
 
-  return filterGender
+    const filterAgeRange = _.filter(filterGender, profile => {
+        const profileBirthday = moment(profile.birthday, 'MM/DD/YYYY');
+        const profileAge = moment().diff(profileBirthday, 'years');
+
+        const withinRangeUser = _.inRange(profileAge, user.ageRange[0], user.ageRange[1] + 1);
+        const withinRangeProfile = _.inRange(userAge, profile.ageRange[0], profile.ageRange[1] + 1);
+        return withinRangeUser && withinRangeProfile;
+    })
+
+    const filtered = _.uniqBy(filterAgeRange, 'uid');
+
+    return filtered;
 }
